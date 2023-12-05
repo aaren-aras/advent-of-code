8a117debe5005e0261d6edcbcaf9f2575f08bae3
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-12-04 21:22:10 -0500

    Add package.json

--- 2023/day-1/trebuchet-1.js
+++ 2023/day-1/trebuchet-1.js
@@ -1,7 +1,7 @@
 // Alternatively, use `const fs = require('fs');` w/ `.js` extension (CommonJS) 
 import fs from 'fs'; // Import `fs` module for filesystem operations (https://tinyurl.com/4f3cabtf)
 
-fs.readFile('trebuchet.txt', 'utf8', (err, data) => { 
+fs.readFile('trebuchet.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
     return;
@@ -11,12 +11,12 @@ fs.readFile('trebuchet.txt', 'utf8', (err, data) => {
   const calibSum = lines.reduce((acc, cur) => {
     let calibStr = cur.replace(/\D/g, ''); // Use regex to remove non-numeric characters
     // console.log(calibStr);
-    let calibVal; 
+    let calibVal;
 
     if (calibStr.length > 2) {
       let firstDigit = calibStr[0];
       let secondDigit = calibStr[calibStr.length - 1];
-      calibVal = Number(firstDigit + secondDigit); 
+      calibVal = Number(firstDigit + secondDigit);
     } else if (calibStr.length === 1) calibVal = Number(calibStr + calibStr);
     else calibVal = Number(calibStr);
     // console.log(calibVal);

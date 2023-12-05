9cd6dd58e8881a06efb33d4aa5d5e73a274c50b2
Aaren Arasaratnam <aaren.aras@gmail.com>
2023-12-04 19:37:48 -0500

    Handle conjoined word edge cases

--- 2023/day-1/trebuchet-2.mjs
+++ 2023/day-1/trebuchet-2.mjs
@@ -8,25 +8,37 @@ fs.readFile('trebuchet.txt', 'utf8', (err, data) => {
 
   const lines = data.split('\n'); 
   const calibSum = lines.reduce((acc, cur) => {
+    // Hint: e.g., 'eighthree' => '83', not '8' (https://tinyurl.com/muzza5z5)
     let calibStr = cur.replace(/one|two|three|four|five|six|seven|eight|nine|\D/g, (match) => {
       switch (match) {
         case 'one':
+          // Handle 'oneightwo', 'oneighthree'?
+          if (cur.includes('eight')) return '18'; // Handle 'oneight' 
           return '1';
         case 'two':
+          // Handle 'twoneight'?
+          if (cur.includes('one')) return '21'; // Handle 'twone'
           return '2';
         case 'three':
+          // Handle 'threeightwo', 'threeightwoneighthree', 'threeighthree'?
+          if (cur.includes('eight')) return '38'; // Handle 'threeight' 
           return '3';
         case 'four':
           return '4';
         case 'five':
+          if (cur.includes('eight')) return '58'; // Handle 'fiveight'
           return '5';
         case 'six':
           return '6';
         case 'seven':
+          if (cur.includes('nine')) return '79'; // Handle 'sevenine'
           return '7';
         case 'eight':
+          if (cur.includes('two')) return '82'; // Handle 'eightwo'
+          if (cur.includes('three')) return '83'; // Handle 'eighthree'
           return '8';
         case 'nine':
+          if (cur.includes('eight')) return '98'; // Handle 'nineight'
           return '9';
         default:
           return '';
@@ -44,5 +56,5 @@ fs.readFile('trebuchet.txt', 'utf8', (err, data) => {
     console.log(calibVal);
     return acc + calibVal;
   }, 0);
-  // console.log(calibSum);
+  console.log(calibSum);
 });
